736f28be1d36016a06afde9286cd47e1392fc0d3
ggarabs <ggarabetti0607@gmail.com>
2025-09-15 21:49:15 -0300

    refactor: modularize main grayscale functions

---
+++ README.md
@@ -0,0 +1,20 @@
+## MVP
+
+### Escopo:
+
+- [ ] O programa deve ser capaz de carregar imagens nos formatos mais comuns, como PNG, JPG e BMP, usando a biblioteca SDL_image.
+- [ ] Deve tratar possíveis problemas, como arquivo não encontrado ou arquivo que não seja um formato de imagem válido.
+- [x] O programa precisa implementar uma função que verifica se a imagem carregada já está em escala de cinza ou se é colorida.
+- [x] Caso a imagem seja colorida, o programa deve converter a imagem para escala de cinza usando a seguinte fórmula: 𝑌 = 0.2125 ∗ 𝑅 + 0.7154 ∗ 𝐺 + 0.0721 ∗ 𝐵.
+- [ ] Janela principal: Deve exibir a imagem que está sendo processada. O tamanho da janela deve se adaptar ao tamanho da imagem carregada e deve iniciar centralizada no monitor principal.
+- [ ] Janela secundária (filha da janela principal): Uma janela de tamanho fixo (definido por você), posicionada ao lado da janela principal. Deve exibir o histograma da imagem e um botão de operação.
+- [ ] Na janela secundária, o programa deve exibir o histograma da imagem.
+- [ ] O programa deve analisar o histograma e exibir as seguintes informações:
+- [ ] Média de intensidade: Classificar a imagem como "clara", "média" ou "escura".
+- [ ] Desvio padrão: Classificar o contraste da imagem como "alto", "médio" ou "baixo".
+- [ ] Na janela secundária, deve haver um botão (desenhado com primitivas da SDL).
+- [ ] Ao clicar no botão, o programa deve equalizar o histograma da imagem, atualizando a imagem exibida na janela principal e o histograma na janela secundária.
+- [ ] O texto do botão deve mudar para refletir a ação (ex.: "Equalizado" / "Original")
+- [ ] O estado do botão deve refletir as ações do usuário (ex.: cor azul para estado "neutro", cor azul claro para estado "mouse em cima do botão", cor azul escuro para estado "botão clicado")
+- [ ] Ao pressionar a tecla S do teclado, o programa deve salvar a imagem atualmente exibida na janela principal em um arquivo chamado output_image.png.
+- [ ] Caso o arquivo output_image.png já exista, o programa deve sobrescrever o arquivo.
--- main.cpp
+++ main.cpp
@@ -5,6 +5,53 @@
 
 using namespace std;
 
+bool is_grayscale_image(SDL_Surface *image){
+        Uint8 *image_pixels = (Uint8*)image->pixels;
+        SDL_PixelFormat image_pixel_format = image->format;
+        const SDL_PixelFormatDetails *format_details = SDL_GetPixelFormatDetails(image_pixel_format);
+        Uint8 bytes_per_pixel = format_details->bytes_per_pixel;
+
+        for(int i = 0; i < image->h; i++){
+                for(int j = 0; j < image->w; j++){
+                        Uint8* src = (Uint8*)image_pixels + i*image->pitch + j*bytes_per_pixel;
+
+                        Uint8 r = src[0], g = src[1], b = src[2];
+
+                        if(!(r == g && g == b)) return false;
+                }
+        }
+        return true;
+}
+
+SDL_Surface* to_grayscale(SDL_Surface* src_image){
+        Uint8 *image_pixels = (Uint8*)src_image->pixels;
+        SDL_PixelFormat src_image_pixel_format = src_image->format;
+        const SDL_PixelFormatDetails *format_details = SDL_GetPixelFormatDetails(src_image_pixel_format);
+        Uint8 bytes_per_pixel = format_details->bytes_per_pixel;
+
+        SDL_Surface *grayscale_image = SDL_CreateSurface(src_image->w, src_image->h, src_image_pixel_format);
+
+        Uint8 *output_pixels = (Uint8*)grayscale_image->pixels;
+
+        SDL_PixelFormat grayscale_image_pixel_format = grayscale_image->format;
+        const SDL_PixelFormatDetails *output_format_details = SDL_GetPixelFormatDetails(grayscale_image_pixel_format);
+
+        for(int i = 0; i < src_image->h; i++){
+                for(int j = 0; j < src_image->w; j++){
+                        Uint8* src = (Uint8*)src_image->pixels + i*src_image->pitch + j*bytes_per_pixel;
+                        Uint8* dst = (Uint8*)grayscale_image->pixels + i*grayscale_image->pitch + j*bytes_per_pixel;
+
+                        const Uint8 color = 0.2125*src[0] + 0.7154*src[1] + 0.0721*src[2];
+
+                        dst[0] = dst[1] = dst[2] = color;
+                }
+        }
+
+        return grayscale_image;
+}
+
+
+
 int main(int argc, char** argv){
         if(argc != 2){
                 cerr << "Número de argumentos inválido! Insira exatamente uma imagem." << endl;
@@ -16,38 +63,19 @@ int main(int argc, char** argv){
         SDL_Surface *input_image;
         input_image = IMG_Load(source_code_filename);
 
+        SDL_Surface* input_image24 = SDL_ConvertSurface(input_image, SDL_PIXELFORMAT_RGB24);
+
         if(!input_image){
                 cerr << "Erro ao abrir o arquivo." << endl;
                 return 1;
         }
 
-        SDL_Surface* input_image24 = SDL_ConvertSurface(input_image, SDL_PIXELFORMAT_RGB24);
-        Uint8 *input_image24_image_pixels = (Uint8*)input_image24->pixels;
+        if(!is_grayscale_image(input_image)){
+                SDL_Surface* output_image = to_grayscale(input_image24);
 
-        SDL_PixelFormat input_pixel_format = input_image24->format;
-        const SDL_PixelFormatDetails *format_details = SDL_GetPixelFormatDetails(input_pixel_format);
-        
-        Uint8 bytes_per_pixel = format_details->bytes_per_pixel;
-
-        SDL_Surface *black_and_white_image = SDL_CreateSurface(input_image24->w, input_image24->h, input_pixel_format);
-
-        Uint8 *output_pixels = (Uint8*)black_and_white_image->pixels;
-
-        SDL_PixelFormat black_and_white_image_pixel_format = black_and_white_image->format;
-        const SDL_PixelFormatDetails *output_format_details = SDL_GetPixelFormatDetails(black_and_white_image_pixel_format);
-
-        for(int i = 0; i < input_image24->h; i++){
-                for(int j = 0; j < input_image24->w; j++){
-                        Uint8* src = (Uint8*)input_image24->pixels + i*input_image24->pitch + j*bytes_per_pixel;
-                        Uint8* dst = (Uint8*)black_and_white_image->pixels + i*black_and_white_image->pitch + j*bytes_per_pixel;
-
-                        const Uint8 color = 0.2125*src[0] + 0.7154*src[1] + 0.0721*src[2];
-
-                        dst[0] = dst[1] = dst[2] = color;
-                }
-        }
-
-        SDL_SaveBMP(black_and_white_image, "black_and_white_image.bmp");
+                SDL_SaveBMP(output_image, "grayscale_image.bmp");
+                SDL_DestroySurface(output_image);
+        } else cout << "Imagem já está em tons de cinza" << endl;
 
         SDL_DestroySurface(input_image);
 
